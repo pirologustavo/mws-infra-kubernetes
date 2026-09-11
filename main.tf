56b7bd24683cb599855c2792841eec6eb2b918dc
Gustavo Pirolo <gusto.pirolo25@hotmail.com>
2026-09-11 06:34:48 -0300

    Validado main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,45 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+data "aws_vpc" "default" {
+  default = true
+}
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.27"
-
-  vpc_id                   = "vpc-placeholder"
-  subnet_ids               = ["subnet-placeholder1", "subnet-placeholder2"]
-  control_plane_subnet_ids = ["subnet-placeholder1", "subnet-placeholder2"]
-
-  eks_managed_node_groups = {
-    mws_nodes = {
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
-      instance_types = ["t3.medium"]
-    }
+data "aws_subnets" "filtered" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-1a", "us-east-1b"]
+  }
+}
+
+resource "aws_eks_cluster" "mws_cluster" {
+  name     = var.cluster_name
+  role_arn = "arn:aws:iam::646417168660:role/LabRole"
+  version  = "1.30"
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.filtered.ids
+  }
+}
+
+resource "aws_eks_node_group" "mws_nodes" {
+  cluster_name    = aws_eks_cluster.mws_cluster.name
+  node_group_name = "mws_nodes"
+  node_role_arn   = "arn:aws:iam::646417168660:role/LabRole"
+  subnet_ids      = data.aws_subnets.filtered.ids
+
+  ami_type        = "AL2_x86_64"
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [
+    aws_eks_cluster.mws_cluster
+  ]
 }
